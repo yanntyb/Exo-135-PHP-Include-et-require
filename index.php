1cff56fe4b9b3b53ba894853f11eecf438ca1728
yann tyb <yanntyb.lbc@gmail.com>
2021-01-19 09:23:40 +0100

    a

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 echo "Un fichier php va être inclu à la suite de ce texte ...<br><br>";
 
 // inclure fichier texte.php ci dessous
-
+include "texte.php";
 
 // Deuxieme Ligne ( inclure fichiers vars.php ci dessous )
-
+require "vars.php";
 
 
 echo "J'aime également les " . $maVar;
